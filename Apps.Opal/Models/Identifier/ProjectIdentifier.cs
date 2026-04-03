264d2063d42d4f044e2a3f20efbc8766892cf064
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-04-03 11:31:36 +0300

    attemp to fix 'Project ID' input being passed incorrectly

--- Apps.Opal/Models/Identifier/ProjectIdentifier.cs
+++ Apps.Opal/Models/Identifier/ProjectIdentifier.cs
@@ -1,9 +1,11 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.Opal.Handlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Opal.Models.Identifier;
 
 public class ProjectIdentifier
 {
-    [Display("Project ID")]
-    public string ProjectId { get; set; }
+    [Display("Project ID"), DataSource(typeof(TestDataHandler))]
+    public string ProjectId { get; set; } = string.Empty;
 }
