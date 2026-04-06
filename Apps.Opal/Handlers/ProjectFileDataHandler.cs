38e25a94c1b061d55b3e753bf2df493ced48320f
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-04-06 15:58:16 +0300

    add validation to ProjectIdentifier, use it in related data handlers

--- Apps.Opal/Handlers/ProjectFileDataHandler.cs
+++ Apps.Opal/Handlers/ProjectFileDataHandler.cs
@@ -3,7 +3,6 @@ using Apps.Opal.Models.Entities;
 using Apps.Opal.Models.Identifier;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
-using Blackbird.Applications.Sdk.Common.Exceptions;
 using Blackbird.Applications.Sdk.Common.Invocation;
 
 namespace Apps.Opal.Handlers;
@@ -15,21 +14,17 @@ public class ProjectFileDataHandler(
 {
     public async Task<IEnumerable<DataSourceItem>> GetDataAsync(DataSourceContext context, CancellationToken ct)
     {
-        if (string.IsNullOrEmpty(projectIdentifier.ProjectId))
-            throw new PluginMisconfigurationException("Please specify the project ID first");
+        projectIdentifier.Validate();
 
-        if (!int.TryParse(projectIdentifier.ProjectId, out var projectId))
-        {
-            throw new PluginMisconfigurationException(
-                @"Please specify a valid project ID integer.
-                The 'Project ID' value must not be passed from outputs of previous actions or events
-                to perform a search during the bird building");
-        }
-
-        var request = new RestRequest($"projects/{projectId}");
+        var request = new RestRequest($"projects/{projectIdentifier.ProjectId}");
         var response = await Client.ExecuteWithErrorHandling<ProjectEntity>(request);
 
         var processedFiles = response.Files.Where(x => x.FileType is "output" or "final");
-        return processedFiles.Select(x => new DataSourceItem(x.FileId, x.ToString())).ToList();
+        return processedFiles
+            .Select(x => new DataSourceItem(x.FileId, x.ToString()))
+            .Where(x =>
+                context.SearchString == null ||
+                x.DisplayName.Contains(context.SearchString, StringComparison.OrdinalIgnoreCase))
+            .ToList();
     }
 }
--- Apps.Opal/Models/Identifier/ProjectIdentifier.cs
+++ Apps.Opal/Models/Identifier/ProjectIdentifier.cs
@@ -1,9 +1,26 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Exceptions;
 
 namespace Apps.Opal.Models.Identifier;
 
 public class ProjectIdentifier
 {
     [Display("Project ID")]
-    public string ProjectId { get; set; } 
+    public string ProjectId { get; set; } = string.Empty;
+
+    public ProjectIdentifier Validate()
+    {
+        if (string.IsNullOrEmpty(ProjectId))
+            throw new PluginMisconfigurationException("Please specify the project ID first");
+
+        if (!int.TryParse(ProjectId, out var projectId))
+        {
+            throw new PluginMisconfigurationException(
+                @"Please specify a valid project ID integer.
+                The 'Project ID' value must not be passed from outputs of previous actions or events
+                to perform a search during the bird building");
+        }
+
+        return this;
+    }
 }
